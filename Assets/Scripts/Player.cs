c2c8207257ba3048df8fc70000d20e0d4d028dd7
papersaccul <59151352+papersaccul@users.noreply.github.com>
2023-04-17 03:31:44 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int playerHealth = 10;
+    [SerializeField] private float playerSpeedMultiplier = 50f;
+    [SerializeField] private float playerJumpForce = 50f;
+    [SerializeField] private float playerMaxJumpHeight = 15f;
+    [SerializeField] private bool isGrounded = false;
+
+    private Rigidbody2D ObjRigidbody;
+    private Animator ObjAnimator;
+    private SpriteRenderer ObjSprite;
+
+    private States State
+    {
+        get { return (States)ObjAnimator.GetInteger("State"); }
+        set { ObjAnimator.SetInteger("State", (int)value); }
+    }
+
+    private void Awake()
+    {
+        ObjRigidbody = GetComponent<Rigidbody2D>();
+        ObjAnimator = GetComponent<Animator>();
+        ObjSprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (isGrounded) State = States.idle;
+
+        if (Input.GetButton("Horizontal"))
+            PlayerRun();
+        if (isGrounded && Input.GetButton("Jump"))
+            PlayerJump();
+        Debug.DrawRay(transform.position, transform.forward * 5f, Color.green);
+    }
+
+    private void FixedUpdate()
+    {
+        IsGroundChecker();
+    }
+
+    private void PlayerRun()
+    {
+        if (isGrounded) State = States.run;
+
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+    
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, playerSpeedMultiplier * Time.deltaTime);
+
+        ObjSprite.flipX = dir.x < 0.0f;
+    }
+
+    private void PlayerJump()
+    {
+        float clampedVerticalSpeed = Mathf.Clamp(ObjRigidbody.velocity.y, -playerMaxJumpHeight, playerMaxJumpHeight);
+        ObjRigidbody.velocity = new Vector2(ObjRigidbody.velocity.x, clampedVerticalSpeed);
+
+        ObjRigidbody.AddForce(Vector2.up * playerJumpForce, ForceMode2D.Impulse);
+    }
+
+    private void IsGroundChecker()
+    {
+        Vector2 currentPosition = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 10.0f; 
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll(currentPosition, direction, distance);
+
+        foreach (RaycastHit2D hit in hits)
+            isGrounded = (hit.collider != null && hit.collider.gameObject != gameObject);
+
+        if (!isGrounded)
+        {
+            if (ObjRigidbody.velocity.y < 0)
+                State = States.fall;
+            else
+                State = States.jump;
+        }
+    }
+
+
+}
+
+public enum States
+{
+    idle,       // 0
+    run,        // 1
+    jump,       // 2
+    fall,       // 3
+    attack1,    // 4
+    attack2,    // 5
+    block,      // 6
+    die,        // 7
+    stop        // 8    
+}
