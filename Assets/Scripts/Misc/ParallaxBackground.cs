223896235bcaa02c82d3b773e1ac9503af28f238
papersaccul <59151352+papersaccul@users.noreply.github.com>
2023-04-23 01:45:38 +0300

    Parallax Effect + cave1 bg sprites

---
+++ Assets/Scripts/Misc/ParallaxBackground.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] Transform followingTarget;
+    [SerializeField, Range(0f, 1f)] float parallaxStrenght = 0.1f;
+    [SerializeField] private bool disableVerticalParallax;
+    Vector3 targetPreviousPosition;
+
+    private void Start()
+    {
+        if (!followingTarget)
+            followingTarget = Camera.main.transform;
+
+        targetPreviousPosition = followingTarget.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 delta = followingTarget.position - targetPreviousPosition;
+
+        if (disableVerticalParallax)
+            delta.y = 0f;
+
+        targetPreviousPosition = followingTarget.position;
+
+        transform.position += delta * parallaxStrenght;
+    }
+}
