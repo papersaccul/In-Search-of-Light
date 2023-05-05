bac52e9c4fad22a19ee69e5739746a8e718bbb81
papersaccul <59151352+papersaccul@users.noreply.github.com>
2023-05-05 17:34:36 +0300

    LightSaber effect sample

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,16 +1,22 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField, Range(1, 3), Description("1 - orthographic, 2 - transform, 3 - rect")] private int type = 1;
     [SerializeField] private Transform player;
     [SerializeField] private float cameraFollowSpeed = 3f;
     [SerializeField] private float minCameraSize = 35f;
     [SerializeField] private float maxCameraSize = 55f;
     [SerializeField] private float cameraSizeSmoothTime = 1.5f;
-
+    [SerializeField] private float positionZ = -15f;
+    [SerializeField] private float offsetY = 6f;
+    [SerializeField] private float offsetX = 0f;
+    
     private new Camera camera;
     private Vector3 playerPosition;
     private float currentCameraSizeVelocity;
@@ -26,13 +32,16 @@ public class CameraController : MonoBehaviour
     private void FixedUpdate()
     {
         playerPosition = player.position;
-        playerPosition.z = -15f;
-        playerPosition.y += 6f;
+        playerPosition.z = positionZ;
+        playerPosition.y += offsetY;
+        playerPosition.x += offsetX;
 
         transform.position = Vector3.Lerp(transform.position, playerPosition, Time.deltaTime * cameraFollowSpeed);
          
         float newCameraSize = Mathf.Lerp(minCameraSize, maxCameraSize, player.GetComponent<Rigidbody2D>().velocity.magnitude / 10f);
-
+        
         camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, newCameraSize, ref currentCameraSizeVelocity, cameraSizeSmoothTime);
+           
+        
     }
 }
